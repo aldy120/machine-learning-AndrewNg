a473c153917acac076d496dce7b5ead638455591
z7 <aldy120@yahoo.com.tw>
2015-11-17 22:46:34 +0800

    add linear regression cost function

--- machine-learning-ex5/ex5/linearRegCostFunction.m
+++ machine-learning-ex5/ex5/linearRegCostFunction.m
@@ -18,7 +18,10 @@ grad = zeros(size(theta));
 %
 %               You should set J to the cost and grad to the gradient.
 %
-
+theta_regularized=theta;
+theta_regularized(1)=0;
+J=(1/(2*m))*sum((X*theta-y).^2)+(lambda/(2*m))*sum(theta_regularized.^2);
+grad=(1/m)*(X'*(X*theta-y))+(lambda/m)*theta_regularized;
 
 
 
