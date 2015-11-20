f555d4f42fb725aceb268c7f395045d9a9d7f91f
z7 <aldy120@yahoo.com.tw>
2015-11-20 21:36:04 +0800

    fix typo

--- machine-learning-ex5/ex5/learningCurve.m
+++ machine-learning-ex5/ex5/learningCurve.m
@@ -55,7 +55,7 @@ error_val   = zeros(m, 1);
 for i=1:m
     theta=trainLinearReg(X(1:i,:),y(1:i),lambda);
     error_train(i)=linearRegCostFunction(X(1:i,:),y(1:i),theta,0);
-    % error_val use all cross-validation set to evaluate.
+    % error_val use all examples in cross-validation set to evaluate.
     error_val(i)=linearRegCostFunction(Xval,yval,theta,0);
 % -------------------------------------------------------------
 
