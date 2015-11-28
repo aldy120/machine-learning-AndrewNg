0dcfc3665c511704c996404033d4226e3a80e979
z7 <aldy120@yahoo.com.tw>
2015-11-28 14:10:04 +0800

    add data3 set parameter

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -22,8 +22,27 @@ sigma = 0.3;
 %  Note: You can compute the prediction error using 
 %        mean(double(predictions ~= yval))
 %
+C_vec=[0.01,0.03,0.1,0.3,1,3,10,30];
+sigma_vec=[0.01,0.03,0.1,0.3,1,3,10,30];
+% evaluate validation error for each C in C_vec and sigma in sigma_vec,
+% store in error_val(i,j), where i and j are the index of C and sigma
+error_val=zeros(length(C_vec),length(sigma_vec));
+for i=1:length(C_vec)
+    for j=1:length(sigma_vec)
+        C=C_vec(i);
+        sigma=sigma_vec(j);
+        model=svmTrain(X,y,C,@(X1,X2)gaussianKernel(X1,X2,sigma));
+        predictions=svmPredict(model,Xval);
+        error_val(i,j)=mean(double(predictions~=yval));
+    end
+end
 
-
+% find the index of C and sigma in C_vec and sigma_vec, and set C and sigma
+% to the parameter for min validation error
+[error_min_vec,C_index]=min(error_val);
+[error_min,sigma_index]=min(error_min_vec);
+sigma=sigma_vec(sigma_index);
+C=C_vec(C_index(sigma_index));
 
 
 
